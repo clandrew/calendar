ae041d8e538fe5fe1f98311530c45aaf8c74ac39
ClAndrew <cmlandrews@gmail.com>
2023-01-31 08:12:34 -0800

    Remove and sort usings

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Windows;
 using System.IO;
 using System.Windows.Forms;
 
@@ -16,9 +9,6 @@ namespace Calendar
     public partial class Form1 : Form
     {
         DateTime displayedMonth;
-        int dayOfWeekStartIndexForThisMonth;
-        int daysThisMonth;
-
         LaidOut laidOut;
 
         public class Entry
